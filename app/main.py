333efd5ce9561c85bbebef531beab8f6a759fac5
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-02-21 22:26:27 +0200

    revised annotation and revised check blocks in methods

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ class Distance:
 
     def __add__(
             self,
-            other: "int | float | Distance"
-    ) -> "Distance":
+            other: int | float | Distance
+    ) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
 
@@ -25,89 +25,90 @@ class Distance:
 
     def __iadd__(
             self,
-            other: "int | float | Distance"
-    ) -> "Distance":
+            other: int | float | Distance
+    ) -> Distance:
         if isinstance(other, Distance):
             self.km += float(other.km)
             return self
-        elif isinstance(other, (int, float)):
+
+        if isinstance(other, (int, float)):
             self.km += float(other)
             return self
-        else:
-            raise TypeError("Not is int of float type")
+
+        raise TypeError("Not is int of float type")
 
     def __mul__(
             self,
             scalar: int | float
-    ) -> "Distance":
+    ) -> Distance:
         if isinstance(scalar, (int, float)):
             return Distance(self.km * scalar)
-        else:
-            raise TypeError("Not is int of float type")
+
+        raise TypeError("Not is int of float type")
 
     def __truediv__(
             self,
             scalar: int | float
-    ) -> "Distance":
+    ) -> Distance:
         if isinstance(scalar, (int, float)) and scalar == 0:
             raise ZeroDivisionError("Division by zero")
 
         if isinstance(scalar, (int, float)):
             return Distance(round(self.km / scalar, 2))
-        else:
-            raise TypeError("Not is int of float type")
+
+        raise TypeError("Not is int of float type")
 
     def __lt__(
             self,
-            other: "int | float | Distance"
+            other: int | float | Distance
     ) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km < other
-        else:
-            return NotImplemented
+
+        return NotImplemented
 
     def __gt__(
             self,
-            other: "int | float | Distance"
+            other: int | float | Distance
     ) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km > other
-        else:
-            return NotImplemented
+
+        return NotImplemented
 
     def __eq__(
             self,
-            other: "int | float | Distance"
+            other: int | float | Distance
     ) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km == other
-        else:
-            return NotImplemented
+
+        return NotImplemented
 
     def __le__(
             self,
-            other: "int | float | Distance"
+            other: int | float | Distance
     ) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km <= other
-        else:
-            return NotImplemented
+
+        return NotImplemented
 
     def __ge__(
             self,
-            other: "int | float | Distance"
+            other: int | float | Distance
     ) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km >= other
-        else:
-            return NotImplemented
+
+        return NotImplemented
